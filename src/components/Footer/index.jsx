399021fe6dcc66fa55ef7e87a106b69f3d6cdeb6
Folarin Lawal <lawalfolarin6@gmail.com>
2022-05-09 14:05:29 +0000

    Initialized project

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-slate-100 py-8">
+      <div className="container flex items-center justify-between py-4 mx-auto">
+        <p className="text-sm">
+          Made by{" "}
+          <span className="font-bold">
+            {" "}
+            <a href="https://madflows.dev" target={"_blank"} rel="noreferrer">
+              Folarin Lawal
+            </a>{" "}
+          </span>{" "}
+          for{" "}
+          <span className="underline decoration-wavy decoration-indigo-700">
+            <a
+              href="https://www.100daysofcode.com/"
+              target={"_blank"}
+              rel="noreferrer"
+            >
+              #100DaysOfCode
+            </a>
+          </span>
+        </p>
+
+        <div className="flex gap-1 items-center justify-center">
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
